897d965f909392af3200d086168fc5052d8292cd
razlandau <landau.raz@gmail.com>
2021-03-22 14:16:43 +0200

    init

---
+++ max_div_seq/submissions/1.py
@@ -0,0 +1,12 @@
+def max_div_seq(n, k):
+    cnt_chk = 0
+    cnt_max = 0
+    n_to_str = str(n)
+    for i in n_to_str:
+        if int(i) % k == 0:
+            cnt_chk += 1
+            cnt_max = max(cnt_chk, cnt_max)
+        else:
+            cnt_chk = 0
+
+    return cnt_max
